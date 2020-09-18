8c958ca598adf4ea47fa6e4aee754346cd67543c
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-09-18 13:54:51 +0900

    postのcreateを定義し、投稿できたことを確認した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,14 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save!
+  end
+
+  private
+  def post_params
+    params.required(:post).permit(:content)
+  end
 end
