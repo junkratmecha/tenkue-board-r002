8b40a3adcea942f5c672eeb8537939e2e0011753
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-09-18 14:19:15 +0900

    バリデーション（presence true、１４０字以内）とエラーメッセージが表示されるようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,11 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save!
+    if @post.save
+      redirect_to post_path
+    else
+      render :new
+    end
   end
 
   private
