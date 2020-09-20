713f68d02d7fbbaafdc7add4ee38b5c599024f3a
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-09-20 17:39:31 +0900

    destroyアクションを定義、投稿を削除できるようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy!
+    redirect_to posts_path, notice: '削除できました'
+  end
+
   private
   def post_params
     params.required(:post).permit(:content)
