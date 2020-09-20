2cb0a82341012443b7635328bf2eeadd15a617a4
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-09-20 16:41:18 +0900

    edit、updateアクションを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,18 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.includes(:user)
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update
+      redirect_to root_path, notice: '編集できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
 end
