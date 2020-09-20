ef110ded37694b6abc44e9233eb96fabe9cc313d
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-09-20 16:10:03 +0900

    routeにeditとupdateを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 
-  resources :posts, only: :index
+  resources :posts, only: [:index, :edit, :update]
 end
