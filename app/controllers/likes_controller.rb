2c7cbdb71811f50ecaf76112dbb7058f71f6eb83
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-09-24 13:59:15 +0900

    viewでいいねの状態で表示を切り替え

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,6 +7,7 @@ class LikesController < ApplicationController
   def destroy
     post = Post.find(params[:post_id])
     like = post.likes.find_by!(user_id: current_user.id)
+    
     like.destroy!
   end
 end
