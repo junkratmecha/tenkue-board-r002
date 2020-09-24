1f2c4f0f22f1eb22ae5eb05a8d287151bfff26ce
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-09-24 13:32:40 +0900

    like_controllerの記述ミスを修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,12 @@
 class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    post.likes.create!(user_id: curent_user.id)  
+    post.likes.create!(user_id: current_user.id)  
   end
 
   def destroy
     post = Post.find(params[:post_id])
-    like = post.likes.find_by!(user_id: curent_user.id)
+    like = post.likes.find_by!(user_id: current_user.id)
     like.destroy!
   end
 end
