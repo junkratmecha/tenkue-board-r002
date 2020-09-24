16aeae8c93cbba452d9f886228c02f961bca65b1
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-09-24 17:20:53 +0900

    いいねを非同期にした

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require rails-ujs
+// = require jquery
+// = require rails-ujs
 //= require activestorage
 //= require bootstrap-sprockets
 //= require data-confirm-modal
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -15,7 +15,7 @@ class LikesController < ApplicationController
 
   private
   def set_post
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
   end
 end
 
