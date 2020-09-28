014bf9220600f2756a97c9361eba14f7cc5d0f53
junkratmecha <junkratmecha@gmail.com>
2020-09-28 20:38:23 +0900

    add validation

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2020_09_20_090716) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
+    t.string "name", default: "転クエ太郎", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
