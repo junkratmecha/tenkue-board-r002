850d25de0587c40d577a349d18855d0d259bf5b9
junkratmecha <junkratmecha@gmail.com>
2020-09-29 14:50:15 +0900

    add post_spec.rb

--- spec/controllers/posts_cotroller_spec.rb
+++ spec/controllers/posts_cotroller_spec.rb
@@ -1,23 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  let(:post)          { create(:post) }
+  let(:post) { create(:post) }
 
-  before do
-    get :show, params: { id: post.id }
+  context 'visit post-index' do
+    before do
+      get :index
+    end
+
+    it "responds successfully" do
+      expect(response).to be_successful
+      expect(response).to have_http_status "200"
+    end
+
+    it "render index page" do
+      expect(response).to render_template :index
+    end
   end
 
-  it "responds successfully" do
-    expect(response).to be_successful
-    expect(response).to have_http_status "200"
-  end
+  context 'visit post-show' do
+    before do
+      get :show, params: { id: post.id }
+    end
 
-  it "render show page" do
-    expect(response).to render_template :show
-  end
+    it "responds successfully" do
+      expect(response).to be_successful
+      expect(response).to have_http_status "200"
+    end
 
-  it "assigns @post" do
-    expect(assigns(:post)).to eq post
-  end
+    it "render show page" do
+      expect(response).to render_template :show
+    end
 
+    it "assigns @post" do
+      expect(assigns(:post)).to eq post
+    end  
+  end
 end
